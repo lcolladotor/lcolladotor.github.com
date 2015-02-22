dc18fd31ca8c94d714fcb48d016f368b968cc03c
rstudio <rstudio@example.com>
2015-02-22 17:54:57 +0000

    general checkin

---
+++ cpp/01_mean.cpp
@@ -0,0 +1,35 @@
+// sourceCpp('./cpp/01_mean.cpp')
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// Below is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar)
+
+// For more on using Rcpp click the Help button on the editor toolbar
+
+// [[Rcpp::export]]
+int timesTwo(int x) {
+   return x * 2;
+}
+
+// [[Rcpp::export]]
+double meanC(NumericVector x) {
+  int n = x.size();
+  double total = 0;
+
+  for(int i = 0; i < n; ++i) {
+    total += x[i];
+  }
+  return total / n;
+}
+
+/*** R
+library(microbenchmark)
+x <- runif(1e5)
+microbenchmark(
+  mean(x),
+  meanC(x)
+)
+*/
