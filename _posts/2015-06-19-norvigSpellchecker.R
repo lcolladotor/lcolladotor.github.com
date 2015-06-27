4de85ef924a787cf55834ddf7d202d3dce2a622f
ttmmghmm <ttmmghmm@gmail.com>
2015-06-27 17:07:51 -0400

    .

--- _posts/2015-06-19-norvigSpellchecker.R
+++ _posts/2015-06-19-norvigSpellchecker.R
@@ -28,3 +28,28 @@ correct("ov")
 correct("cakke")
 ## [1] "cake"
 
+# Read in big.txt, a 6.5 mb collection of different English texts.
+raw_text <- paste(readLines("http://www.norvig.com/big.txt"), collapse = " ")
+# Make the text lowercase and split it up creating a huge vector of word tokens.
+split_text <- strsplit(tolower(raw_text), "[^a-z]+")
+# Count the number of different type of words.
+word_count <- table(split_text)
+# Sort the words and create an ordered vector with the most common type of words first.
+sorted_words <- names(sort(word_count, decreasing = TRUE))
+
+correct <- function(word) {
+  # Calculate the edit distance between the word and all other words in sorted_words.
+  edit_dist <- adist(word, sorted_words)
+  # Calculate the minimum edit distance to find a word that exists in big.txt 
+  # with a limit of two edits.
+  min_edit_dist <- min(edit_dist, 2)
+  # Generate a vector with all words with this minimum edit distance.
+  # Since sorted_words is ordered from most common to least common, the resulting
+  # vector will have the most common / probable match first.
+  proposals_by_prob <- c(sorted_words[ edit_dist <= min(edit_dist, 2)])
+  # In case proposals_by_prob would be empty we append the word to be corrected...
+  proposals_by_prob <- c(proposals_by_prob, word)
+  # ... and return the first / most probable word in the vector.
+  proposals_by_prob[1]
+}
+
